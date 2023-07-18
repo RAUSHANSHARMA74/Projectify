7278053cf70b1d51b07eaa9398d72d14277f03c6
Raushan Sharma <raushansharma7462@gmail.com>
2023-07-18 20:39:31 +0530

    connected to mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 const express = require("express")
+const {connection} = require("./config/connection")
+require("dotenv").config()
+
 const app = express()
 
 
@@ -7,9 +10,11 @@ app.use(express.json())
 const port = process.env.port || 1481
 app.listen(port, async ()=>{
     try {
-        
+        await connection
+        console.log("connected to mongodb")
     } catch (error) {
-        console.log("something went wrong in port")
+        console.log("something went wrong in connected to database")
+        console.log(error)
     }
     console.log(`server is running on port ${port}`)
 })
