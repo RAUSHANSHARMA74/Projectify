f836898ae96a09dd034de34395f49cbadde0e1db
Raushan Sharma <raushansharma7462@gmail.com>
2023-07-21 01:47:41 +0530

    employee  auth

--- backend/authorization/managerAuth.js
+++ backend/authorization/managerAuth.js
@@ -1,28 +1,26 @@
-const {Managers} = require("../model/managerModel")
-require("dotenv").config()
-const jwt = require("jsonwebtoken")
+const { Managers } = require("../model/managerModel");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
-
-let managerAuth = async  (req, res, next) =>{
-    let  [bearer, token] = req.headers.authorization.split(" ")
-    // console.log(token)
-    if(!token){
-        res.send({"message" : "Not Authorized"})
-        return
-    }else{
-        var decoded = jwt.verify(token, "ratham");
-        if(!decoded){
-            res.send({"message" : "Not Authorized"})
-        }else{
-            let deenData = await Managers.findOne({email : decoded.manager })
-            if(deenData!=null){
-                next()
-            }else{
-                res.send({"message" : "Not Authorized"})
-            }
-        }
+let managerAuth = async (req, res, next) => {
+  let [bearer, token] = req.headers.authorization.split(" ");
+  // console.log(token)
+  if (!token) {
+    res.send({ message: "Not Authorized" });
+    return;
+  } else {
+    var decoded = jwt.verify(token, "ratham");
+    if (!decoded) {
+      res.send({ message: "Not Authorized" });
+    } else {
+      let deenData = await Managers.findOne({ email: decoded.manager });
+      if (deenData != null) {
+        next();
+      } else {
+        res.send({ message: "Not Authorized" });
+      }
     }
-}
+  }
+};
 
-
-module.exports = {managerAuth}
+module.exports = { managerAuth };
