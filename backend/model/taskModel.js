aa73ea7a7f0afddb00394795e16b682abffac7ae
Raushan Sharma <raushansharma7462@gmail.com>
2023-07-22 21:57:07 +0530

    backend taskModel modify schema

--- backend/model/taskModel.js
+++ backend/model/taskModel.js
@@ -1,17 +1,16 @@
-
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const taskSchema = mongoose.Schema({
-    taskName : {type: String, require:true},
-    description : String,
-    status : String,
-    startDate : {type: Date, required: true},
-    endDate : {type: Date, required: true},
-    project_id : String,
-    employee_id : String,
-    resource_id : Array
+  taskName: { type: String, require: true },
+  description: String,
+  status: String,
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  project: { type: mongoose.Schema.Types.ObjectId, ref: "Projects" },
+  employee: { type: mongoose.Schema.Types.ObjectId, ref: "Employees" },
+  resources: [{ type: mongoose.Schema.Types.ObjectId, ref: "Resources" }],
 });
 
-const Tasks = mongoose.model('Tasks', taskSchema)
+const Tasks = mongoose.model("Tasks", taskSchema);
 
-module.exports = {Tasks}
+module.exports = { Tasks };
